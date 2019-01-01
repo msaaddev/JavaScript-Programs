f8ed478f7867d522555d7730980a34316f4f96a0
mr-saad <mohammadsaad1612@gmail.com>
2019-01-02 01:20:03 +0500

    📖 commit

---
+++ Sum of Odd digits in a number/main.js
@@ -0,0 +1,15 @@
+
+function calc(num) {
+    var sum = 0;
+    var rem;
+    var number = document.getElementById("input").value;
+    while (number != 0) {
+        rem = number % 10;
+        if (rem % 2 != 0) {
+            sum = sum + rem;
+        }
+        number = (number - rem) / 10;
+        
+    }
+    document.getElementById("result").value = sum; 
+}
